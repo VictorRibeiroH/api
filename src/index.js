a13150ad5f968dedaf91c111ef0959ebc15fd503
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-09-07 06:17:45 -0300

    delete CRUD

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ const typeDefs = gql`
 
     createTaskList(title: String!): TaskList!
     updateTaskList(id: ID!, title: String!): TaskList!
+    deleteTaskList(id: ID!): Boolean
   }
 
   input SignUpInput {
@@ -161,6 +162,16 @@ const resolvers = {
         }
       );
       return await db.collection("TaskList").findOne({ _id: ObjectID(id) });
+    },
+
+    deleteTaskList: async (_, { id }, { db, user }) => {
+      if (!user) {
+        throw new Error("Authencation Error. Por favor Faça login");
+      }
+
+      await db.collection("TaskList").removeOne({ _id: ObjectID(id) });
+
+      return true;
     },
   },
 
